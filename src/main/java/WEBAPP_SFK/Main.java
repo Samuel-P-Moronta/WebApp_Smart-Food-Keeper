2f57198606b73abe6c363c93f744efe7c91fa736
SMORONTA <20170570@ce.pucmm.edu.do>
2021-10-30 00:27:01 -0400

    Javalin microframework first steps created

--- src/main/java/WEBAPP_SFK/Main.java
+++ src/main/java/WEBAPP_SFK/Main.java
@@ -1,4 +1,40 @@
 package WEBAPP_SFK;
 
+import WEBAPP_SFK.services.connect.DataBaseServices;
+import io.javalin.Javalin;
+import io.javalin.core.util.RouteOverviewPlugin;
+import io.javalin.plugin.rendering.JavalinRenderer;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
+
+import java.sql.SQLException;
+
 public class Main {
+    private static String connectionMode = "";
+    public static void main(String[] args) throws SQLException {
+        String msg = "Smart Food Keeper - Final Project";
+        System.out.println(msg);
+        if(args.length >=1){
+            connectionMode = args[0];
+            System.out.println("Operation Mode: "+connectionMode);
+        }
+        if(connectionMode.isEmpty()){
+            //Database init
+            DataBaseServices.getInstance().initDB();
+        }
+
+        Javalin app = Javalin.create(config -> {
+
+            config.addStaticFiles("/public");
+            config.registerPlugin(new RouteOverviewPlugin("/routesControl"));
+            config.enableCorsForAllOrigins();
+
+            JavalinRenderer.register(JavalinThymeleaf.INSTANCE, ".html");
+
+        });
+        app.start(7000);
+
+    }
+    public static String getModoConexion() {
+        return connectionMode;
+    }
 }
